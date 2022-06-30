11106d501556fdc48fc71aae14d0a3795d0fd923
gvaghsiya02 <gvaghasiya02@gmail.com>
2022-06-30 11:10:13 +0530

    first commit

---
+++ src/test/java/com/azure/springBootDocker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.springBootDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
